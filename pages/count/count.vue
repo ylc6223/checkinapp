3fb10ce3ada9cca76cd0ddf951a4c0e22f9eaeb0
Jarry Yang <260902108@qq.com>
2023-01-11 18:24:24 +0800

    Initial commit

---
+++ pages/count/count.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="container bg-gray flex flex-column items-center">
+		<view class="mt-20 card w-300 flex flex-column">
+			<view class="flex justify-between">
+				<image class="avatar" src="../../static/icons/checkin-active.png" mode=""></image>
+				<!-- #ifdef H5 || MP-WEIXIN -->
+					<tui-tabs class="toggle-btns" :tabs="tabs" :currentTab="currentTab" @change="change" width="120" height="60" itemWidth="33%"></tui-tabs>
+				<!-- #endif -->
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tabs: [{
+					name: "日"
+				}, {
+					name: "周"
+				}, {
+					name: "月"
+				}],
+				currentTab:0,
+			};
+		},
+		methods:{
+			change({index}){
+				this.currentTab=index
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.card{
+	height: calc(100vh - 70px);
+	min-height: 400px;
+}
+.avatar{
+	width: 50px;
+	height: 50px;
+}
+.toggle-btns{
+
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="container bg-gray flex flex-column items-center">
+		<view class="mt-20 card w-300 flex items-center">
+			<image class="avatar" src="../../static/icons/checkin-active.png" mode=""></image>
+			<view class="names flex-1">
+				<text class="block">Jerry</text>
+				<text class="block">研发部</text>
+			</view>
+			<view class="actions">
+				<button class="action-btn flex justify-center items-center">
+					<text class="icon">&#xe6da;</text>
+				</button>
+				<text class="block">{{value}}</text>
+			</view>
+		</view>
+		
+		<view class="mt-20 h-full card w-300 flex flex-column">
+			<view class="checkin-states flex justify-between">
+				<view class="flex-1 flex flex-column bg-gray rounded-md p-10">
+					<text>上班09:00</text>
+					<text>未打卡</text>
+				</view>
+				<view class="flex-1 flex flex-column bg-gray rounded-md p-10">
+					<text>下班09:00</text>
+					<text>未打卡</text>
+				</view>
+			</view>
+			
+			<!-- 打卡操作 -->
+			<view class="checkin-btn mx-auto rounded-full flex flex-column justify-center items-center">
+				<text class="text-white">下班打卡</text>
+				<text>15:36:03</text>
+			</view>
+			<!-- 打卡tips -->
+			<view class="tips mx-auto">
+				<text class="icon">&#xe72d;</text>
+				<text>已进入考勤范围:深蓝工业园</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				iconType: ['success'],
+			}
+		},
+		onLoad() {
+        // #ifdef APP-PLUS|| MP-WEIXIN
+        this.iconType = ['success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search','clear']
+        // #endif
+        // #ifdef MP-ALIPAY
+        this.iconType = ['info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear', 'success', 'success_no_circle', 'loading']
+        // #endif
+        // #ifdef MP-BAIDU
+        this.iconType = ['success', 'info', 'warn', 'waiting', 'success_no_circle', 'clear', 'search', 'personal', 'setting', 'top', 'close', 'cancel', 'download', 'checkboxSelected', 'radioSelected', 'radioUnselect']
+        // #endif
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.avatar{
+	width: 50px;
+	height: 50px;
+}
+.names{
+	margin-left: 10px;
+	margin-right: 20px;
+}
+.action-btn{
+	width: 50px;
+	height: 50px;
+}
+.card:last-child{
+	height: calc(100vh - 160px);
+	min-height: 400px;
+}
+.checkin-states>view:first-child{
+	margin-right: 5px;
+}
+.checkin-states>view:last-child{
+	margin-left: 5px;
+}
+.checkin-btn{
+	width: 150px;
+	height: 150px;
+	margin-top: 40px;
+	box-shadow: 0 4px 10px -1px rgba(185,147, 214,1), 0 2px 8px -2px rgba(140,166,219,1);
+	background: #00d2ff;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to right, #3a7bd5, #00d2ff);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to right, #3a7bd5, #00d2ff); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+.tips{
+	margin-top: 20px;
+}
+
+</style>
